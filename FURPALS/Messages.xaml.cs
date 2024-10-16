9e84f422e0fb651d150a2b5d60036fcd37558ee0
jamestapawan <tapawan.306172@dasmarinas.sti.edu.ph>
2024-10-16 18:48:27 +0800

    Messages Page

---
+++ FURPALS/Messages.xaml.cs
@@ -0,0 +1,36 @@
+namespace Messages
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBackButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnHomeButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnPlusButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnMessageButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnSettingsButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
