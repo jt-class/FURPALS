cfd6c2614cb63296532b059303b05556580d5e83
Lpalma7 <offfkanggg@gmail.com>
2024-10-20 18:13:34 +1100

    uploaded login.cs

--- FURPALS/obj/Debug/net8.0-android/FURPALS.AssemblyInfo.cs
+++ FURPALS/obj/Debug/net8.0-android/FURPALS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FURPALS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+11ebfcc1002a40ca0b3f274f9b5698914ea944ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+173458d391d7d0018103cadaa8f4e4c40da147f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FURPALS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FURPALS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
